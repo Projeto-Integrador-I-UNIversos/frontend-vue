da0f1a278c8f4aba762f484e5c4f4c55dded12dd
LaraVic9 <laravicspereira@gmail.com>
2024-08-04 13:13:36 -0300

    add tela cadastrar livro

--- src/views/auth/login/components/FormsLogin.vue
+++ src/views/auth/login/components/FormsLogin.vue
@@ -1,46 +1,9 @@
-<template>
-    <div className='containerRight'>
-        <form class="form" @submit.prevent="loadUser">
-      <h1 class="title">Seja Bem-Vindo!</h1>
-      <h3>Crie agora sua conta</h3>
-
-      <div class="formInputs">
-        <div class="inputContainer">
-          <i class="material-icons icon">mail</i>
-          <input placeholder="E-mail" class="form-control" v-model="email"></input>
-        </div>
-
-        <div class="inputContainer">
-          <i class="material-icons icon">lock</i>
-          <input placeholder="Senha" class="form-control" v-model="senha" required type="password"></input>
-        </div>
-
-      </div>
-
-      <div class="row">
-        <div class="col">
-          <Checkbox />
-        </div>
-        <div class="col forgotContainer">
-          <p class="forgotPassword">Esqueceu a senha?</p>
-        </div>
-      </div>
-
-      <div class="buttonContainer">
-        <button class="button" type="submit">Entrar</button>
-      </div>
-
-      <p class="haveAccount">Ja possui uma conta? <Link to="/login" class="haveAccount">Entrar</Link></p>
-    </form>
-    </div>
-</template>
-
-<script>
-import { defineComponent, ref } from 'vue';
+<script lang="ts">
+import { defineComponent } from 'vue';
 import axios from 'axios';
 import '../style/FormsStyle.scss';
 
-export default {
+export default defineComponent({
     name: 'FormsLogin',
     data() {
         return {
@@ -67,9 +30,47 @@ export default {
             });
                 }
             }
-}
+})
 </script>
 
+<template>
+  <div className='containerRight'>
+      <form class="form" @submit.prevent="loadUser">
+    <h1 class="title">Seja Bem-Vindo!</h1>
+    <h3>Crie agora sua conta</h3>
+
+    <div class="formInputs">
+      <div class="inputContainer">
+        <i class="material-icons icon">mail</i>
+        <input placeholder="E-mail" class="form-control" v-model="email"></input>
+      </div>
+
+      <div class="inputContainer">
+        <i class="material-icons icon">lock</i>
+        <input placeholder="Senha" class="form-control" v-model="senha" required type="password"></input>
+      </div>
+
+    </div>
+
+    <div class="row">
+      <div class="col">
+        <Checkbox />
+      </div>
+      <div class="col forgotContainer">
+        <p class="forgotPassword">Esqueceu a senha?</p>
+      </div>
+    </div>
+
+    <div class="buttonContainer">
+      <button class="button" type="submit">Entrar</button>
+    </div>
+
+    <p class="haveAccount">Ja possui uma conta? <Link to="/login" class="haveAccount">Entrar</Link></p>
+  </form>
+  </div>
+</template>
+
+
 <style>
-/* Adicione seus estilos aqui */
+
 </style>
