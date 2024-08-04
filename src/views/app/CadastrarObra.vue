f9388f141a2ee2b189ce30fd256f129d3c97885d
LaraVic9 <laravicspereira@gmail.com>
2024-08-04 13:20:39 -0300

    add label for input obra

--- src/views/app/CadastrarObra.vue
+++ src/views/app/CadastrarObra.vue
@@ -2,6 +2,7 @@
 import { defineComponent } from 'vue';
 import { Input } from '../../components/ui/input';
 import axios from 'axios';
+import Label from '../../components/ui/label/Label.vue';
 /**
  * titulo
  * idioma
@@ -18,6 +19,7 @@ export default defineComponent({
     name: 'CadastrarObra',
     components: {
         Input,
+        Label
     },
     data() {
         return {
@@ -65,13 +67,21 @@ export default defineComponent({
 <template>
     <div>
         <p>cadastrar obra</p>
+        <Label for="email">Titulo</Label>
         <Input type="text" placeholder="Titulo"/>
+        <Label for="email">Iidoma</Label>
         <Input type="text" placeholder="Idioma"/>
+        <Label for="email">Paginas</Label>
         <Input type="text" placeholder="Paginas"/>
+        <Label for="email">Pais</Label>
         <Input type="text" placeholder="Pais"/>
+        <Label for="email">Descricao</Label>
         <Input type="text" placeholder="Descricao"/>
+        <Label for="email">Status</Label>
         <Input type="text" placeholder="status"/>
+        <Label for="email">Generos</Label>
         <Input type="text" placeholder="Generos"/>
+        <Label for="email">PDF Livro</Label>
         <Input id="picture" type="file" />
     </div>
 </template>
